ee2fcc56a1f3a2f526da2cf055dfaf1c23a80711
leonardoPDB <105393119+leonardoPDB@users.noreply.github.com>
2023-04-22 16:45:48 -0300

    Add files via upload
    Adicionado tabela de esforço

--- ficha.js
+++ ficha.js
@@ -10,6 +10,11 @@ function salvarFicha() {
     carisma: document.getElementById('carisma').value,
     habilidades: document.getElementById('habilidades').value,
     itens: document.getElementById('itens').value,
+    basico: document.getElementById('basico').value,
+    arma: document.getElementById('arma').value,
+    armaFogo: document.getElementById('armaFogo').value,
+    magia: document.getElementById('magia').value,
+    supremo: document.getElementById('supremo').value,
   }
   localStorage.setItem('ficha', JSON.stringify(ficha))
   alert('Ficha salva com sucesso!')
@@ -28,6 +33,11 @@ function carregarFicha() {
     document.getElementById('carisma').value = ficha.carisma
     document.getElementById('habilidades').value = ficha.habilidades
     document.getElementById('itens').value = ficha.itens
+    document.getElementById('basico').value = ficha.basico
+    document.getElementById('arma').value = ficha.arma
+    document.getElementById('armaFogo').value = ficha.armaFogo
+    document.getElementById('magia').value = ficha.magia
+    document.getElementById('supremo').value = ficha.supremo
   }
 }
 
@@ -43,5 +53,50 @@ function rolarAtributo(idAtributo) {
   // Descobre onde colocar o resultado
   const resultado = document.getElementById('resultado' + idAtributo)
   // Mostra o Resultado
+  resultado.innerHTML = valorTotal
+}
+
+function rolarBasico(idAtributo) {
+  const inputAtributo = document.getElementById(idAtributo)
+  const valorAtributo = Number(inputAtributo.value)
+  const valorDado = Math.floor(Math.random() * 4) + 1
+  const valorTotal = valorAtributo + valorDado
+  const resultado = document.getElementById('resultado' + idAtributo)
+  resultado.innerHTML = valorTotal
+}
+
+function rolarArma(idAtributo) {
+  const inputAtributo = document.getElementById(idAtributo)
+  const valorAtributo = Number(inputAtributo.value)
+  const valorDado = Math.floor(Math.random() * 6) + 1
+  const valorTotal = valorAtributo + valorDado
+  const resultado = document.getElementById('resultado' + idAtributo)
+  resultado.innerHTML = valorTotal
+}
+
+function rolarArmaFogo(idAtributo) {
+  const inputAtributo = document.getElementById(idAtributo)
+  const valorAtributo = Number(inputAtributo.value)
+  const valorDado = Math.floor(Math.random() * 8) + 1
+  const valorTotal = valorAtributo + valorDado
+  const resultado = document.getElementById('resultado' + idAtributo)
+  resultado.innerHTML = valorTotal
+}
+
+function rolarMagia(idAtributo) {
+  const inputAtributo = document.getElementById(idAtributo)
+  const valorAtributo = Number(inputAtributo.value)
+  const valorDado = Math.floor(Math.random() * 10) + 1
+  const valorTotal = valorAtributo + valorDado
+  const resultado = document.getElementById('resultado' + idAtributo)
+  resultado.innerHTML = valorTotal
+}
+
+function rolarSupremo(idAtributo) {
+  const inputAtributo = document.getElementById(idAtributo)
+  const valorAtributo = Number(inputAtributo.value)
+  const valorDado = Math.floor(Math.random() * 12) + 1
+  const valorTotal = valorAtributo + valorDado
+  const resultado = document.getElementById('resultado' + idAtributo)
   resultado.innerHTML = valorTotal
 }
